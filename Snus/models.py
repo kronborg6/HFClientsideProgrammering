eb5b81acc4c2fc5b87e01da6565efd5ef1489cc6
Kronborg <mkronborg7@gmail.com>
2020-10-28 10:52:40 +0100

    0.3.5
    gik igang med at lave table til Kontak, Produt Table

--- Snus/models.py
+++ Snus/models.py
@@ -1,8 +1,22 @@
 from django.db import models
-
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 class SnusProdut(models.Model):
-    pass
+    Navn = models.CharField(max_length=100)
+    ProdutName = models.CharField(max_length=100)
+    nikotinindhold = models.IntegerField(max_length=10)
+    Prise = models.FloatField(max_length=100)
+    Description = models.TextField()
+
+
 
 class Kontak(models.Model):
-    pass
+    Fornavn = models.CharField(max_length=100)
+    Efternavn = models.CharField(max_length=100)
+    Email = models.CharField(max_length=60)
+    message = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.Fornavn + " " + self.Efternavn
