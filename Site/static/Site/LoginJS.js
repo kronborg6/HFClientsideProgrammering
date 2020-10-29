dc12ed7019df61498e9fc8c747ec025df2da7706
Kronborg <mkronborg7@gmail.com>
2020-10-29 23:31:57 +0100

    0.5.5

--- Site/static/Site/LoginJS.js
+++ Site/static/Site/LoginJS.js
@@ -2,8 +2,16 @@ var modal = document.getElementById('id01');
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-    if (event.target == modal) {
+    if (event.target === modal) {
         modal.style.display = "none";
+    }
+}
+var Reg = document.getElementById('id02');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target === Reg) {
+        Reg.style.display = "none";
     }
 }
 
